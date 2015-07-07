575e291f5c47495d76b34b21b08a8736c1e942ae
ece asilar <ece.asilar@cern.ch>
2015-07-07 16:21:52 +0200

    samples_13TeV_74X path set

--- CMGTools/TTHAnalysis/cfg/crab_with_das/heppy_samples.py
+++ CMGTools/TTHAnalysis/cfg/crab_with_das/heppy_samples.py
@@ -1,14 +1,16 @@
 #-------- SAMPLES AND TRIGGERS -----------
+#from CMGTools.TTHAnalysis.samples.samples_13TeV_PHYS14 import *
+from CMGTools.RootTools.samples.samples_13TeV_74X import *
 
-from CMGTools.TTHAnalysis.samples.samples_13TeV_PHYS14 import *
+
 #selectedComponents =  [TTJets]
 #TTJets.splitFactor=1000
-from CMGTools.TTHAnalysis.samples.samples_13TeV_private_heplx import *
-selectedComponents = [T2DegStop_300_270]
+#from CMGTools.TTHAnalysis.samples.samples_13TeV_private_heplx import *
+#selectedComponents = [T2DegStop_300_270]
 
 
-from CMGTools.TTHAnalysis.samples.samples_13TeV_private_heplx import *
-selectedComponents = [DYJetsToLL_M50_PU20bx25]#, DYJetsToLLHT100to200_M50_PU20bx25, DYJetsToLLHT200to400_M50_PU20bx25, DYJetsToLLHT400to600_M50_PU20bx25, DYJetsToLLHT600toInf_M50_PU20bx25]
+#from CMGTools.TTHAnalysis.samples.samples_13TeV_private_heplx import *
+#selectedComponents = [DYJetsToLL_M50_PU20bx25]#, DYJetsToLLHT100to200_M50_PU20bx25, DYJetsToLLHT200to400_M50_PU20bx25, DYJetsToLLHT400to600_M50_PU20bx25, DYJetsToLLHT600toInf_M50_PU20bx25]
 
 #selectedComponents = [ TT_PU40bx25 ]
 selectedComponents = [ TT_PU4bx50 ]
