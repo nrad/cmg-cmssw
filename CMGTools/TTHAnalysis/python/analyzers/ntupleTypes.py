bb4974e831f7e84bae602a7586506329f190c248
schoefbeck <robert.schoefbeck@cern.ch>
2015-09-24 12:24:30 +0200

    adding genParticleWithMotherIndex and genJetType

--- CMGTools/TTHAnalysis/python/analyzers/ntupleTypes.py
+++ CMGTools/TTHAnalysis/python/analyzers/ntupleTypes.py
@@ -171,6 +171,19 @@ metTypeSusy = NTupleObjectType("metSusy", baseObjectTypes = [ metType ], variabl
 ## GENPARTICLE
 ##------------------------------------------  
 
+genParticleWithMotherIndex = NTupleObjectType("genParticleWithMotherIndex", baseObjectTypes = [ genParticleWithMotherId ], mcOnly=True, variables = [
+    ## these work for 74X miniaod
+    NTupleVariable("nDaughters", lambda x : x.numberOfDaughters(), int, help="index of the daughters in the genParticles"),
+    NTupleVariable("nMothers", lambda x : x.numberOfMothers(), int, help="index of the mother in the genParticles"),
+    NTupleVariable("motherIndex1", lambda x : x.motherRef(0).index() if x.numberOfMothers() > 0 else -1, int, help="index of the first mother in the genParticles"),
+    NTupleVariable("daughterIndex1", lambda x : x.daughterRef(0).index() if x.numberOfDaughters() >0 else -1, int, help="index of the first mother in the genParticles"),
+    NTupleVariable("motherIndex2", lambda x : x.motherRef(x.numberOfMothers()-1).index() if x.numberOfMothers() > 1 else -1, int, help="index of the last mother in the genParticles"),
+    NTupleVariable("daughterIndex2", lambda x : x.daughterRef(x.numberOfDaughters()-1).index() if x.numberOfDaughters() > 1 else -1, int, help="index of the last mother in the genParticles"),
+])
+
+genJetType = NTupleObjectType("genJets",  baseObjectTypes = [ fourVectorType ], mcOnly=True, variables = [
+    NTupleVariable("nConstituents", lambda x : x.nConstituents() ,help="Number of Constituents"),
+])
 
 ##------------------------------------------  
 ## SECONDARY VERTEX CANDIDATE
